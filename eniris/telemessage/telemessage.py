69fee7061966856ba452411b0faab44ab317e84e
Pieter Stroobant <pieter@eniris.be>
2023-07-31 14:08:55 +0200

    Got rid of redundant properties of Telemessage

--- eniris/telemessage/telemessage.py
+++ eniris/telemessage/telemessage.py
@@ -5,12 +5,10 @@ class Telemessage(object):
   """A message with lines of telemetry which must be transmitted as a single block (telemetry messages), including url parameters which must be included in the transmission, and the number of failed transmission attempts"""
   parameters: dict[str, str]
   lines: list[bytes]
-  nrRetries: int
 
-  def __init__(self, parameters: dict[str, str], lines: list[bytes], nrRetries: int = 0):
+  def __init__(self, parameters: dict[str, str], lines: list[bytes]):
     self.parameters = parameters
     self.lines = lines
-    self.nrRetries = nrRetries
 
   def toJson(self):
     return asdict(self)
