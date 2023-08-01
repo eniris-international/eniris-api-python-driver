9674f2fea3ad1643e94ac1807fdaefda62cc56f4
Pieter Stroobant <pieter@eniris.be>
2023-08-01 09:01:11 +0200

    Added improved PooledTelemessageWriter import

--- eniris/telemessage/writer/__init__.py
+++ eniris/telemessage/writer/__init__.py
@@ -1,2 +1,3 @@
 from eniris.telemessage.writer.writer import *
 from eniris.telemessage.writer.direct import DirectTelemessageWriter
+from eniris.telemessage.writer.pooled import PooledTelemessageWriter
