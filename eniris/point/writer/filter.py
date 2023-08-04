b7e25c313a9c3c3e718860f8d10986470a85c20b
Pieter Stroobant <pieter@eniris.be>
2023-08-04 20:04:52 +0200

    Improved PointDuplicateFilter to allow point storage over multiple write operations, with built in options for limiting memory usage

--- eniris/point/writer/filter.py
+++ eniris/point/writer/filter.py
@@ -1,13 +1,45 @@
-from threading import Lock
+from threading import RLock
+import time
+from collections import OrderedDict
 
 from eniris.point import Point
 from eniris.point.writer import PointWriterDecorator, PointWriter
 
+NANOSECOND_CONVERSION = 10**9
+PointDuplicateFilterMemoryKey = tuple[frozenset[tuple[str, str]], str, frozenset[tuple[str, str]]]
+
+def createPointDuplicateFilterMemoryKey(point: Point) -> PointDuplicateFilterMemoryKey:
+  namespace_parameters = point.namespace.toUrlParameters() 
+  frozen_namespace = frozenset((key, namespace_parameters[key]) for key in namespace_parameters)
+  frozen_tags = frozenset((key, point.tags[key]) for key in point.tags)
+  return (frozen_namespace, point.measurement, frozen_tags)
+
 class PointDuplicateFilter(PointWriterDecorator):
-  def __init__(self, output: PointWriter):
+  def __init__(self, output: PointWriter, maximumEntryAgeS=2*24*3600, maximumSeriesEntryCount=500_000, maximumEntryCount=1_000_000_000):
     super().__init__(output)
-    self.memory: dict[tuple[frozenset[tuple[str, str]], str, frozenset[tuple[str, str]]], dict[tuple[int, str], bool|int|float|str]] = dict() # {(frozen_namespace, measurement, frozen_tagset) => (time, fieldKey) => fieldValue}
-    self.lock = Lock()
+    self.maximumEntryAgeS = maximumEntryAgeS
+    self.maximumEntryCount = maximumEntryCount
+    self.maximumSeriesEntryCount = maximumSeriesEntryCount
+    self.memory: dict[PointDuplicateFilterMemoryKey, OrderedDict[int, dict[str, bool|int|float|str]]] = dict()
+    self.entryKey2updateTs: OrderedDict[tuple[PointDuplicateFilterMemoryKey, int], int] = OrderedDict()
+    self.lock = RLock()
+
+  def deleteExpiredEntries(self):
+    with self.lock:
+      thresholdTimestamp = int((time.time()-self.maximumEntryAgeS)*NANOSECOND_CONVERSION)
+      while len(self.entryKey2updateTs) > 0:
+        (pointDuplicateFilterMemoryKey, pointTimestamp) = next(iter(self.entryKey2updateTs))
+        updateTimestamp = self.entryKey2updateTs[(pointDuplicateFilterMemoryKey, pointTimestamp)]
+        if updateTimestamp > thresholdTimestamp:
+          break
+        self._delete(pointDuplicateFilterMemoryKey, pointTimestamp)
+
+  def _delete(self, pointDuplicateFilterMemoryKey: PointDuplicateFilterMemoryKey, pointTimestamp: int):
+    del self.entryKey2updateTs[(pointDuplicateFilterMemoryKey, pointTimestamp)]
+    pointDuplicateFilterMemoryValue = self.memory[pointDuplicateFilterMemoryKey]
+    del pointDuplicateFilterMemoryValue[pointTimestamp]
+    if len(pointDuplicateFilterMemoryValue) == 0:
+      del self.memory[pointDuplicateFilterMemoryKey]
 
   def writePoints(self, points: 'list[Point]'):
     """Filter that filters out measurements that have been pushed before by looking if new measurements are identical to last pushed measurements.
@@ -22,40 +54,36 @@ class PointDuplicateFilter(PointWriterDecorator):
       return
     out: 'list[Point]' = []
     with self.lock:
-      # Calculate the new memory entries
-      new_memory_entries: 'dict[tuple[frozenset[tuple[str, str]], str, frozenset[tuple[str, str]]], dict[tuple[int, str], bool|int|float|str]]' = dict()
-      for dp in points:
-        namespace_parameters = dp.namespace.toUrlParameters() 
-        frozen_namespace = frozenset((key, namespace_parameters[key]) for key in namespace_parameters)
-        frozen_tags = frozenset((key, dp.tags[key]) for key in dp.tags)
-        key = (frozen_namespace, dp.measurement, frozen_tags)
-        if key not in new_memory_entries:
-          new_memory_entries[key] = dict()
-        new_memory_row_key = new_memory_entries[key]
-        for field_name in dp.fields:
-          field_value = dp.fields[field_name]
-          field_key = (int(dp.time.timestamp()*10**9), field_name)
-          new_memory_row_key[field_key] = field_value
-      # Create a list of measurements with updates
-      for dp in points:
-        namespace_parameters = dp.namespace.toUrlParameters() 
-        frozen_namespace = frozenset((key, namespace_parameters[key]) for key in namespace_parameters)
-        frozen_tags = frozenset((key, dp.tags[key]) for key in dp.tags)
-        key = (frozen_namespace, dp.measurement, frozen_tags)
-        selected_fields = dict()
-        if key not in self.memory:
-          self.memory[key] = dict()
-        old_memory_row_key = self.memory[key]
-        new_memory_row_key = new_memory_entries[key]
-        for field_name in dp.fields:
-          field_value = dp.fields[field_name]
-          field_key = (int(dp.time.timestamp()*10**9), field_name)
-          if new_memory_row_key[field_key] == field_value and (field_key not in old_memory_row_key or old_memory_row_key[field_key] != field_value):
-            old_memory_row_key[field_key] = field_value
-            selected_fields[field_name] = field_value
-        if len(selected_fields) > 0:
-          out.append(Point(dp.namespace, dp.measurement, dp.time, dp.tags, selected_fields))
-      # Update the memory
-      self.memory.update(new_memory_entries)
+      self.deleteExpiredEntries()
+      currentTs = int(time.time()*NANOSECOND_CONVERSION)
+      for p in points:
+        pTs = int(p.time*NANOSECOND_CONVERSION) if p.time is not None else currentTs
+        pMemoryKey = createPointDuplicateFilterMemoryKey(p)
+        # Add an entry for the fields of p to the data structure
+        self.entryKey2updateTs[(pMemoryKey, pTs)] = currentTs
+        self.entryKey2updateTs.move_to_end((pMemoryKey, pTs))
+        if pMemoryKey not in self.memory:
+          self.memory[pMemoryKey] = OrderedDict()
+        pMemoryValue = self.memory[pMemoryKey]
+        if pTs not in pMemoryValue:
+          pMemoryValue[pTs] = dict()
+        memoryFields = pMemoryValue[pTs]
+        # Make sure the count constraints are not violated
+        while len(pMemoryValue) > self.maximumSeriesEntryCount:
+          p2Ts = next(iter(pMemoryValue))
+          self._delete(pMemoryKey, p2Ts)
+        while len(self.entryKey2updateTs) > self.maximumEntryCount:
+          (p2MemoryKey, p2Ts) = next(iter(self.entryKey2updateTs))
+          self._delete(p2MemoryKey, p2Ts)
+        # Figure out which fields were actually updated
+        updatedFields = dict()
+        for fieldKey in p.fields:
+          fieldValue = p.fields[fieldKey]
+          if fieldKey not in memoryFields or memoryFields[fieldKey] != fieldValue:
+            memoryFields[fieldKey] = fieldValue
+            updatedFields[fieldKey] = fieldValue
+        # If necessary, add a point to the output list
+        if len(updatedFields) > 0:
+          out.append(Point(p.namespace, p.measurement, p.time, p.tags, updatedFields))
     if len(out) > 0:
       self.output.writePoints(out)
