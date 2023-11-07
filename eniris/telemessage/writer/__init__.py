8a2362524dcac925ce6ed5251278cb779c98ca1b
Wim Verheirstraeten <wim@eniris.be>
2023-11-07 19:52:48 +0100

    Bugfix

--- eniris/telemessage/writer/__init__.py
+++ eniris/telemessage/writer/__init__.py
@@ -1,4 +1,5 @@
-from eniris.telemessage.writer.writer import TelemessageWriter, TelemessagePrinter, GZipTelemessageWriter
+from eniris.telemessage.writer.writer import TelemessageWriter, TelemessagePrinter
+from eniris.telemessage.writer.gzipped import GZipTelemessageWriter
 from eniris.telemessage.writer.direct import DirectTelemessageWriter
 from eniris.telemessage.writer.pooled import PooledTelemessageWriter
 
