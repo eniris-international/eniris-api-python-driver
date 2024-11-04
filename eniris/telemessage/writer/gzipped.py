4f2828d3bd9aa0703ece19189758739311ba18cf
Wim Verheirstraeten <wim@eniris.be>
2024-11-04 13:15:13 +0100

    Optimize the gzip writer

--- eniris/telemessage/writer/gzipped.py
+++ eniris/telemessage/writer/gzipped.py
@@ -5,7 +5,12 @@ import gzip
 
 
 class GZipTelemessageWriter:
-    """TelemessageWriter that compresses Telemessages using the gzip algorithm, and then passes them on to another TelemessageWriter"""
+    """
+    TelemessageWriter that compresses Telemessages using the gzip algorithm,
+    and then passes them on to another TelemessageWriter. If the compression does
+    not have a net positive effect in the amount of data send, then the
+    original uncompressed message is sent.
+    """
 
     def __init__(self, output: TelemessageWriter, compresslevel: int = 9):
         """Optional argument is the compression level, in range of 0-9.
@@ -23,7 +28,13 @@ class GZipTelemessageWriter:
             message.parameters, gzippedData, headers=ammendedHeaders
         )
 
-        self.output.writeTelemessage(gzippedMessage)
+        if len(message.data) > len(gzippedData) + 23:
+            # If the gzipped message is more efficient, then write this
+            # 23 is the amount of bytes used for the Content-Encoding header
+            self.output.writeTelemessage(gzippedMessage)
+        else:
+            # If it is not more efficient, then don't use compression
+            self.output.writeTelemessage(message)
         
     def close(self):
         self.output.close()
